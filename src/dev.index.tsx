e68f4810a64acd4b35de32c66ac9fa27375f910b
zeonardolima <zeonardo.lima@original.io>
2022-08-12 00:33:34 -0300

    improve dev index file

--- src/dev.index.tsx
+++ src/dev.index.tsx
@@ -1,14 +1,34 @@
 import Tab from './components/tab'
 import Tab2 from './components/tab/tab2'
 
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+const TabController: React.FC = () => {
+  const [active, setActive] = useState(1)
+
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const index = +event.currentTarget.value
+    setActive(index)
+  }
+
+  return (
+    <div data-tab={active}>
+      <Tab className='controlled' active={active} onActiveChange={onClick} theme={{ color: { primary: '#0f0' } }}>
+        <Tab.Pane title='First Pane'>First Pane Body</Tab.Pane>
+        <Tab.Pane title='Second Pane'>Second Pane Body</Tab.Pane>
+        <Tab.Pane title='Third Pane'>Third Pane Body</Tab.Pane>
+      </Tab>
+    </div>
+  )
+}
 
 root.render(
   <React.StrictMode>
     <h1>ZTABS</h1>
-    <Tab className='uncontrolled' initialActive={0} theme={{ color: { primary: '#00f' } }}>
+    <h2>uncontrolled</h2>
+    <Tab className='uncontrolled' initialActive={0} theme={{ color: { primary: '#f00' } }}>
       <Tab.Pane title='First Pane'>
         <>
           <strong>First</strong> Pane Body
@@ -22,6 +42,10 @@ root.render(
       <Tab.Pane title='Third Pane'>Third Pane Body</Tab.Pane>
     </Tab>
     <hr />
+    <h2>controlled</h2>
+    <TabController />
+    <hr />
+    <h2>Tab2</h2>
     <Tab2 className='uncontrolled' initialActive={0} theme={{ color: { primary: '#00f' } }}>
       <Tab2.Pane title='First Pane'>
         <>
